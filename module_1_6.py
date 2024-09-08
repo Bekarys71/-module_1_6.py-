e185fcd0d86330b791424cf73fc301ed53f0cb1b
Bekarys71 <Beybarys.imashev@mail.ru>
2024-09-08 16:38:49 +0600

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Bekarys': 1998 , 'Janarys': 2009 , 'Mukhtar': 1967 }
+print(my_dict)
+print(my_dict.get('Bekarys' , 'Bars'))
+my_dict.update({'Bars': 1993 ,
+                'Balkhash' : 1967} )
+print(my_dict)
+my_dict.pop('Bars')
+print(my_dict)
+my_set_= {1,2,3,3,3,3,44,5}
+print(my_set_)
+print(my_set_.add(7777, ))
+print(my_set_)
+print(my_set_.remove(7777, ))
+print(my_set_)
+
+
